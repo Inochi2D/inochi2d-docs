4c01d1b31cfc8743af574a95dcfdfe51f6f56245
seagetch <sigetch@gmail.com>
2023-04-27 01:49:26 +0900

    Removed translation making scripts.

--- docs/make_translations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for LANG_DIR in en source/locale/*; do
-    POLANG=`basename $LANG_DIR`
-    echo "Generating translations $POLANG"
-    SPHINX_LANGUAGE=$POLANG pipenv run make html
-done
