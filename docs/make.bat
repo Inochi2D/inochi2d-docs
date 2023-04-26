6984665c4f65e2169d2fa89213312c9bd3b1204f
LunaTheFoxgirl <cli-luna@protonmail.com>
2023-04-26 12:02:44 +0200

    Fix make.bat for Windows Store Python

--- docs/make.bat
+++ docs/make.bat
@@ -4,13 +4,10 @@ pushd %~dp0
 
 REM Command file for Sphinx documentation
 
-if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
-)
 set SOURCEDIR=source
 set BUILDDIR=build
 
-%SPHINXBUILD% >NUL 2>NUL
+python -m sphinx.cmd.build >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
 	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
@@ -25,11 +22,11 @@ if errorlevel 9009 (
 
 if "%1" == "" goto help
 
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+python -m sphinx.cmd.build -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
 :help
-%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+python -m sphinx.cmd.build -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
 :end
 popd
